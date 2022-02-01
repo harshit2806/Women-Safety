d9b53e21a806d92b557568dd3ee03b2b46d65890
Harshit <harshitsoni2806@gmail.com>
2022-02-01 12:25:29 +0530

    .

---
+++ Women-Safety-App-Android-Project-Source-Code-master/app/build/generated/source/buildConfig/debug/com/prabhu/womensafetyapp/BuildConfig.java
@@ -0,0 +1,11 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.prabhu.womensafetyapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.prabhu.womensafetyapp";
+  
+}
+
