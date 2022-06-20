b05b26faff97c5dff1c04c6d62454e0490b26d75
Harshit <harshitsoni2806@gmail.com>
2022-06-20 17:57:58 +0530

    .

---
+++ Women-Safety-App-Android-Project-Source-Code-master/app/src/main/java/com/prabhu/womensafetyapp/Register.java
@@ -0,0 +1,46 @@
+package com.prabhu.womensafetyapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Register extends Activity {
+	
+	EditText name,number;
+    
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register);
+		
+
+	}
+	
+	
+	
+		
+		db.close();
+			
+		}
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+
+
+}
