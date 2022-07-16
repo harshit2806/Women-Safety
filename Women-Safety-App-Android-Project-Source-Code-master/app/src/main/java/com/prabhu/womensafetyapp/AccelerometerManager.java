6397ac40f00da5ca7427493d710fcc9c9414b3f5
Harshit <harshitsoni2806@gmail.com>
2022-07-16 22:11:21 +0530

    .

--- Women-Safety-App-Android-Project-Source-Code-master/app/src/main/java/com/prabhu/womensafetyapp/AccelerometerManager.java
+++ Women-Safety-App-Android-Project-Source-Code-master/app/src/main/java/com/prabhu/womensafetyapp/AccelerometerManager.java
@@ -48,29 +48,88 @@ public class AccelerometerManager {
     }
   
     /**
-     * Returns true if at least one Accelerometer sensor is available
+     * The listener that listen to events from the accelerometer listener
      */
- 
-         
-         
-    }
+    private static SensorEventListener sensorEventListener =
+        new SensorEventListener() {
   
-    /**
-     * Configures threshold and interval
-     * And registers a listener and start listening
-     * @param accelerometerListener
-     *             callback for accelerometer events
-     * @param threshold
-     *             minimum acceleration variation for considering shaking
-     * @param interval
-     *             minimum interval between to shake events
-     */
-    public static void startListening(
-            AccelerometerListener accelerometerListener,
-            int threshold, int interval) {
-        configure(threshold, interval);
-        startListening(accelerometerListener);
-    }
+        private long now = 0;
+        private long timeDiff = 0;
+        private long lastUpdate = 0;
+        private long lastShake = 0;
   
+        private float x = 0;
+        private float y = 0;
+        private float z = 0;
+        private float lastX = 0;
+        private float lastY = 0;
+        private float lastZ = 0;
+        private float force = 0;
+  
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+  
+        public void onSensorChanged(SensorEvent event) {
+            // use the event timestamp as reference
+            // so the manager precision won't depends
+            // on the AccelerometerListener implementation
+            // processing time
+            now = event.timestamp;
+  
+            x = event.values[0];
+            y = event.values[1];
+            z = event.values[2];
+  
+            // if not interesting in shake events
+            // just remove the whole if then else block
+          if (lastUpdate == 0) {
+                lastUpdate = now;
+                lastShake = now;
+                lastX = x;
+                lastY = y;
+                lastZ = z;
+                Toast.makeText(aContext,"No Motion detected",
+                   Toast.LENGTH_SHORT).show();
+                 
+              } else {
+                timeDiff = now - lastUpdate;
+               
+                if (timeDiff > 0) {
+                     
+                    
+                    force = Math.abs(x + y + z - lastX - lastY - lastZ);
+                     
+                    if (Float.compare(force, threshold) >0 ) {
+                        
+                         
+                        if (now - lastShake >= interval) {
+                             
+                            // trigger shake event
+                            listener.onShake(force);
+                        }
+                        else
+                        {
+                            Toast.makeText(aContext,"No Motion detected.",
+                                Toast.LENGTH_SHORT).show();
+                             
+                        }
+                        lastShake = now;
+                    }
+                    lastX = x;
+                    lastY = y;
+                    lastZ = z;
+                    lastUpdate = now;
+                }
+                else
+                {
+                    Toast.makeText(aContext,"No Motion detected", Toast.LENGTH_SHORT).show();
+                     
+                }
+            } 
+    
+            // trigger change event
+            listener.onAccelerationChanged(x, y, z);
+        }
+  
+    };
   
 }
